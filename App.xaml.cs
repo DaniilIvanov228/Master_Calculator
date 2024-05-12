8cf3f95072152766a37dcaeeccfdf78221ec29a3
User <User@DESKTOP-9CNRDGG>
2024-05-12 21:53:46 +0900

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            if (Preferences.ContainsKey("isDark"))
+            {
+                App.Current.UserAppTheme = Preferences.Get("isDark", false) == true ? AppTheme.Dark : AppTheme.Light ;
+            }
+            else
+            {
+                App.Current.UserAppTheme = AppTheme.Light;
+                Preferences.Set("isDark", false);
+            }
+
+            if (Preferences.ContainsKey("FirstTime"))
+            {
+                MainPage = new AppShell();
+            }
+            else
+            {
+                MainPage = new WelocmeScreen();
+            }
+            
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+
+namespace Calculator
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            //Подлючение шрифтов, настройка и инициализация небходимых данных платформы
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>().UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Montserrat-Medium.ttf", "medium");
+                    fonts.AddFont("Montserrat-Regular.ttf", "regular");
+                    fonts.AddFont("Montserrat-SemiBold.ttf", "semiBold");
+                    fonts.AddFont("Montserrat-Bold.ttf", "bold");
+                });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
